ccabce2e4c99e115d721169aa2ba05a5c288315f
Matthew-Brennan <matthew.brennan4@hotmail.com>
2025-04-04 20:58:07 -0400

    skills

---
+++ src/data/data.js
@@ -0,0 +1,11 @@
+export const skills = [
+    {
+        name: "Apex, Visualforce, Lightning Web Components",
+        name: "Agile Development Methodology",
+        name: "Data Visualization",
+        name: "Project and System Design",
+        name: "API Integration",
+        name: "Dashboard design",
+        
+    }
+]
